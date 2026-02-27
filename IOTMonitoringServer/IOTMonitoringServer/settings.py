1ccb8965c300cbbecb60592dee2807be2ffef7fc
Willian Andres Piarpuzan <w.piarpuzan@uniandes.edu.co>
2026-02-26 21:06:49 -0500

    Se modifica Ips Publicas de Broker, Postgres y Visualizador

--- IOTMonitoringServer/IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/IOTMonitoringServer/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "32.192.26.1",  # Dirección IP de la base de datos
+        "HOST": "44.201.1.227",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
